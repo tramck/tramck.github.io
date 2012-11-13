4fcbae78774a451c9940f4675716023f75c292dd
Travis McKinney <travis.r.mckinney@gmail.com>
2012-11-13 01:06:35 -0500

    sidebar socialize

--- js/custom.js
+++ js/custom.js
@@ -5,7 +5,6 @@ function likes(){
 	$('#about-image img').attr('src', '/images/about/' + number + '.jpeg');
 	$('#likes').html(images[number-1] + '.<br> What else does Travis like?');
 }
-
 $(document).ready(function(){
 	$('pre').addClass("prettyprint linenums");
 	$('code').addClass("prettyprint");
@@ -34,8 +33,57 @@ $(document).ready(function(){
 		$('.workitem').addClass('filtered');
 		$(filterType).removeClass('filtered');
 	});
-
+	var text;
 	
+	$('.sidebar a').hover( function(e){
+		var username = $(this).attr('data-hover');
+		text = $(this).html();
+		$(this).html(username);
+		var eltop = $(this).offset().top;
+		var elbottom = eltop + $(this).height();
+		var cursor = e.pageY;
+		var middle = ($(this).height() / 2) + eltop;
+		console.log("bottom " + elbottom);
+		console.log("middle " + middle);
+		console.log("top " + eltop);
+		
+		if(cursor < middle){
+			$(this).addClass('entertop');
+			var that = this;
+			setTimeout(function(){
+				$(that).removeClass('entertop');
+			}, 500);
+		}
+		else{
+			$(this).addClass('enterbottom');
+			var that = this;
+			setTimeout(function(){
+				$(that).removeClass('enterbottom');
+			}, 500);
+		}
+		
+	}, function(e){
+		$(this).html(text);
+		var eltop = $(this).offset().top;
+		var elbottom = eltop + $(this).height();
+		var cursor = e.pageY;
+		
+		if(cursor > elbottom){
+			$(this).addClass('leavebottom');
+			var that = this;
+			setTimeout(function(){
+				$(that).removeClass('leavebottom');
+			}, 500);
+		}
+		else{
+			$(this).addClass('leavetop');
+			var that = this;
+			setTimeout(function(){
+				$(that).removeClass('leavetop');
+			}, 500);
+		}
+		
+	});
 	
 	
 });
