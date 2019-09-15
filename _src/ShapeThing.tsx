e060ff36f7786929b8f7617f6acc744ee9dd7859
Travis McKinney <travis@beeswax.com>
2019-09-15 19:17:52 -0400

    working on icosahedral faces

--- _src/ShapeThing.tsx
+++ _src/ShapeThing.tsx
@@ -31,8 +31,8 @@ const generateRandomVectors = (n: number): Vector[] => {
         arr.push({
             x: randBetween(0, W),
             y: randBetween(0, H),
-            dx: randBetween(1, 3),
-            dy: randBetween(1, 3),
+            dx: randBetween(-3, 3),
+            dy: randBetween(-3, 3),
         });
     }
     return arr;
@@ -43,34 +43,81 @@ interface Props {
     strokeColor: string;
 }
 
-const ICOSAHEDRON_ADJACENCY_MATRIX = [
-    [0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0],
-    [1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0],
-    [0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1],
-    [0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1],
-    [1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0],
-    [0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1],
-    [1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0],
-    [1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0],
-    [0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1],
-    [0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0],
-    [0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0],
+const ICOSAHEDRON_EDGES = [
+    [0, 1],
+    [0, 4],
+    [0, 6],
+    [0, 7],
+    [0, 10],
+    [1, 4],
+    [1, 5],
+    [1, 7],
+    [1, 9],
+    [2, 3],
+    [2, 6],
+    [2, 8],
+    [2, 10],
+    [2, 11],
+    [3, 6],
+    [3, 7],
+    [3, 9],
+    [3, 11],
+    [4, 5],
+    [4, 8],
+    [4, 10],
+    [5, 8],
+    [5, 9],
+    [5, 11],
+    [6, 7],
+    [6, 10],
+    [7, 9],
+    [8, 10],
+    [8, 11],
+    [9, 11],
 ];
 
-const generateIcosahedralEdges = (vectors: Vector[]): Edge[] => {
-    const edges = [];
-    for (let i = 0; i < ICOSAHEDRON_ADJACENCY_MATRIX.length; i++) {
-        for (let j = i; j < ICOSAHEDRON_ADJACENCY_MATRIX[i].length; j++) {
-            if (ICOSAHEDRON_ADJACENCY_MATRIX[i][j]) {
-                let vi = vectors[i];
-                let vj = vectors[j];
-                edges.push([ vi.x, vi.y, vj.x, vj.y ]);
-            }
-        }
-    }
-    return edges;
-}
+const ICOSAHEDRON_FACES = [
+    [ 0, 5, 1 ],
+    [ 0, 7, 3 ],
+    [ 1, 20, 4 ],
+    [ 2, 24, 3 ],
+    [ 2, 25, 4 ],
+    [ 5, 18, 6 ],
+    [ 6, 22, 8 ],
+    [ 7, 26, 8 ],
+    [ 9, 14, 10 ],
+    [ 9, 17, 13 ],
+    [ 10, 25, 12 ],
+    [ 11, 27, 12 ],
+    [ 11, 28, 13 ],
+    [ 14, 24, 15 ],
+    [ 15, 26, 16 ],
+    [ 16, 29, 17 ],
+    [ 18, 21, 19 ],
+    [ 19, 27, 20 ],
+    [ 21, 28, 23 ],
+    [ 22, 29, 23 ],
+];
+// for (let i = 0; i < ICOSAHEDRON_EDGES.length; i++) {
+//     let [a, b] = ICOSAHEDRON_EDGES[i];
+//     for (let j = 0; j < ICOSAHEDRON_EDGES.length; j++) {
+//         let [b_, c] = ICOSAHEDRON_EDGES[j];
+//         if (b_ === b) {
+//             for (let k = 0; k < ICOSAHEDRON_EDGES.length; k++) {
+//                 let [a_, c_] = ICOSAHEDRON_EDGES[k];
+//                 if (a_ === a && c_ === c) {
+//                     triangles.push([i, j, k]);
+//                 }
+//             }
+//         }
+//     }
+// }
+
+const generateIcosahedralEdges = (vectors: Vector[]): Edge[] =>
+    ICOSAHEDRON_EDGES.map(([i, j]) => [
+        vectors[i].x, vectors[i].y, vectors[j].x, vectors[j].y
+    ]
+);
 
 export default ({ animated, strokeColor }: Props) => {
     const [vectors, setVectors] = useState<Vector[]>(generateRandomVectors(12));
